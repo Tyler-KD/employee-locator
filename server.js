c2af4b63fc96e162b2b01d9f14ba2838d3af392c
unknown <tyler.kd.knapp@gmail.com>
2024-02-11 00:11:13 -0600

    Installed console.table module and created view all departments function

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const inquirer = require('inquirer');
 // Import mysql
 const mysql = require('mysql2');
+// Imports console.table
+const cTable = require('console.table');
 
 // Specify which port the Express.js server will run.
 // process.env.PORT stores the port number on which a web server should listen for incoming connections.
@@ -28,7 +30,7 @@ const db = mysql.createConnection(
     }
 )
 
-// Function for the main menu
+// Function for initializing the main menu inquirer prompts
 function init() {
     inquirer.prompt([
         {
@@ -36,38 +38,60 @@ function init() {
             message: "What would you like to do?",
             name: "action",
             choices: [
+                "View All Departments",
+                "Add Department",
+                "View All Roles",
+                "Add a Role",
                 "View All Employees",
                 "Add Employee",
                 "Update Employee Role",
-                "View All Roles",
-                "Add a Role",
-                "View All Departments",
-                "Add Department",
                 "Quit",
             ]
         }
     ])
-    .then(answer => {
-        switch (answer.action) {
-            case "View All Employees":
-                view_Employees();
-            case "Add Employee":
-                add_Employee();
-            case "View All Roles":
-                view_AllRoles();
-            case "Add a Role":
-                add_Role();
-            case "View All Departments":
-                view_allDepartments();
-            case "Add Department":
-                add_Department();
-            case "Quit":
-                db.end();
-                console.log("Goodbye!");
-        }
-    })
+        .then(answer => {
+            // Instead of writing many if...else statements, the switch statement selects one of many code blocks to be executed.
+            // If there is a match between the value of the expression with the values of each case, then the associated block of code is executed.
+            switch (answer.action) {
+                case "View All Departments":
+                    view_allDepartments();
+                    // The break stops the execution of more code and case testing inside the block.
+                    break;
+                case "View All Roles":
+                    view_AllRoles();
+                    break;
+                case "View All Employees":
+                    view_Employees();
+                    break;
+                case "Add Department":
+                    add_Department();
+                    break;
+                case "Add Employee":
+                    add_Employee();
+                    break;
+                case "Add a Role":
+                    add_Role();
+                    break;
+                case "Update Employee Role":
+                    update_employeeRole();
+                case "Quit":
+                    // Ends connection to database
+                    db.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        })
 };
 
+// View all departments
+function view_allDepartments() {
+    db.query("SELECT * from department", function (err, results) {
+        // The console.table() static method displays tabular data as a table
+        console.table(results);
+        // Return to main menu
+        init();
+    })
+};
 
 // Call to initialize the app
 init();
